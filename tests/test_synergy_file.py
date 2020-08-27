9fd93fc7136df9b9d6046a7b032b2bcef3208bd1
Wrzlprmft <Wrzlprmft@users.noreply.github.com>
2020-08-27 16:37:18 +0200

    Implementing test for multiple observables.

--- tests/test_synergy_file.py
+++ tests/test_synergy_file.py
@@ -61,3 +61,51 @@ def test_time_series(filename,temperature_ts):
 	assert read.results["t at Max V"]["H11","OD:600"] == 5050
 	assert read.results["t at Max V"]["H11"] == 5050
 
+
+@mark.parametrize(
+		  "filename,                         temperature_ts",
+	[
+		( "column.txt"          , True  ),
+	])
+def test_multiple_observables(filename,temperature_ts):
+	data = SynergyFile(path.join("multiple_observables",filename))
+	assert len(data)==1
+	read = data[0]
+	
+	assert read.metadata["Software Version"] == (3,2,1)
+	assert read.metadata["Experiment File Path"] == r"C:\foo.xpt"
+	assert read.metadata["Protocol File Path"] == r"C:\bar.prt"
+	assert read.metadata["Plate Number"] == "Plate 1"
+	assert read.metadata["Reader Type"] == "Synergy H1"
+	assert read.metadata["Reader Serial Number"] == "18092726"
+	assert read.metadata["Reading Type"] == "Reader"
+	assert read.metadata["procedure"] == "foo\nbar\nquz"
+	
+	assert read.metadata["datetime"] == datetime(2020,7,24,15,27,56)
+	
+	assert read.channels == ["OD:600","485,528","530,580"]
+	assert read.rows == list("ABCDEFGH")
+	assert read.cols == list(range(1,13))
+	
+	for channel,offset in [ ("OD:600",108), ("485,528",159), ("530,580",228) ]:
+		times = read.times[channel]
+		assert times[0] == offset
+		assert times[-1] == offset+20*60
+		assert np.all( np.diff(times)==5*60 )
+	
+	if temperature_ts:
+		for channel in read.channels:
+			temps = read.temperatures[channel]
+			assert len(read.times[channel]) == len(temps)
+			assert all( temp==30.0 for temp in temps)
+		assert read.temperature_range == (30.0,30.0)
+	else:
+		TODO
+	
+	assert read["B12" ,"485,528"][2] == 48
+	assert read["B",12,"485,528"][2] == 48
+	assert read["b12" ,"485,528"][2] == 48
+	
+	assert read["H",12,"530,580"][4] == 28
+	assert read["C",2,"OD:600"][4] == 0.085
+
