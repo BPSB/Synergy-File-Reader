b7e5fa8f63e56fdb7ebc8049e57be7421c9c36a9
Wrzlprmft <Wrzlprmft@users.noreply.github.com>
2020-08-31 13:18:26 +0200

    Setting up documentation

---
+++ docs/conf.py
@@ -0,0 +1,53 @@
+import sys
+import os
+from setuptools_scm import get_version
+from unittest.mock import MagicMock as Mock
+
+# Mocking to make RTD autobuild the documentation.
+autodoc_mock_imports = ['numpy']
+#sys.modules.update([("numpy", Mock())])
+sys.path.insert(0,os.path.abspath("../synergy_file_reader"))
+sys.path.insert(0,os.path.abspath("../examples"))
+
+needs_sphinx = '1.6'
+
+extensions = [
+	'sphinx.ext.autodoc',
+	'sphinx.ext.autosummary',
+	'matplotlib.sphinxext.plot_directive',
+	'numpydoc',
+]
+
+source_suffix = '.rst'
+
+master_doc = 'index'
+
+project = 'Synergy File Reader'
+copyright = '2020, Gerrit Ansmann'
+
+release = version = get_version(root='..', relative_to=__file__)
+
+default_role = "any"
+
+add_function_parentheses = True
+
+add_module_names = False
+
+html_theme = 'nature'
+pygments_style = 'colorful'
+htmlhelp_basename = 'Synergy_File_Reader_doc'
+
+plot_html_show_formats = False
+plot_html_show_source_link = False
+
+numpydoc_show_class_members = False
+autodoc_member_order = 'bysource'
+
+def on_missing_reference(app, env, node, contnode):
+	if node['reftype'] == 'any':
+		return contnode
+	else:
+		return None
+
+def setup(app):
+	app.connect('missing-reference', on_missing_reference)
---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+Synergy File Reader for Python
+==============================
+
+This allows you to read text files produced by Synergy plate readers in Python.
+
+Example
+-------
+
+Preparing Files
+---------------
+
+Under the Hood
+--------------
+
+Command Reference
+-----------------
+
+.. automodule:: _synergy_file_reader
+	:members: SynergyResult, SynergyPlate, SynergyFile
